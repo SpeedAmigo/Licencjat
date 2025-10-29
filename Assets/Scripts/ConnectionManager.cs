2af77e2054a2001c7ae16a0cf615b5665a3c52e0
SpeedAmigo <Kxipi7@gmail.com>
2025-10-29 19:59:27 +0100

    steam overlay and code invitation

--- Assets/Scripts/ConnectionManager.cs
+++ Assets/Scripts/ConnectionManager.cs
@@ -10,10 +10,11 @@ public class ConnectionManager : MonoBehaviour
     
     [SerializeField] private string sceneName;
     
-    [SerializeField] private TMP_InputField _inputField;
+    [SerializeField] private TMP_InputField inputField;
     [SerializeField] private FishySteamworks.FishySteamworks fishySteamworks;
-
-    private string _hostHex;
+    [SerializeField] private LobbyManager lobbyManager;
+    
+    private string _lobbyData;
 
     private void Awake()
     {
@@ -24,24 +25,53 @@ public class ConnectionManager : MonoBehaviour
         Instance = this;
         
         DontDestroyOnLoad(this);
+        
+        lobbyManager.evtCreated.AddListener(OnLobbyCreated);
+        lobbyManager.evtEnterSuccess.AddListener(StartConnection);
+        
+        Heathen.SteamworksIntegration.API.Overlay.Client.EventGameLobbyJoinRequested.AddListener(JoinLobby);
     }
-    
-    public void StartHost()
-    {
-        var user = UserData.Get();
-        _hostHex = user.ToString();
 
-        fishySteamworks.StartConnection(true);
+    private void JoinLobby(LobbyData lobbyData, UserData userData)
+    {
+        lobbyManager.Join(lobbyData);
+        
+        if (!userData.IsValid)
+        {
+            Debug.LogError("hostUser is not valid");
+            return;
+        }
+        
+        fishySteamworks.SetClientAddress(userData.id.ToString());
         fishySteamworks.StartConnection(false);
         
-        //fishySteamworks.NetworkManager.SceneManager.LoadGlobalScenes(new SceneLoadData(sceneName));
         SceneManager.LoadScene(sceneName);
     }
 
-    public void StartConnection()
+    private void OnLobbyCreated(LobbyData lobbyData)
     {
-        _hostHex = _inputField.text;
-        var hostUser = UserData.Get(_hostHex);
+        _lobbyData = lobbyData.HexId;
+    }
+
+    public void StartHost()
+    {
+        fishySteamworks.StartConnection(true);
+        fishySteamworks.StartConnection(false);
+        
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void TryJoinLobby()
+    {
+        var lobbyId = inputField.text;
+        
+        lobbyManager.Join(lobbyId);
+    }
+
+    private void StartConnection(LobbyData lobbyData) 
+    {
+        var hostHex = lobbyData.Owner.user.HexId;
+        var hostUser = UserData.Get(hostHex);
 
         if (!hostUser.IsValid)
         {
@@ -54,9 +84,9 @@ public class ConnectionManager : MonoBehaviour
         
         SceneManager.LoadScene(sceneName);
     }
-
+    
     public static string GetHostHex()
     {
-        return Instance._hostHex;
+        return Instance._lobbyData;
     }
 }
