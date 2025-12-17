af47ee6abf906230ede7e4ea4a5cbfbe1d3bb8ea
SpeedAmigo <Kxipi7@gmail.com>
2025-12-17 18:10:05 +0100

    console text done

--- Assets/Scripts/SpaceShipConsoleScript.cs
+++ Assets/Scripts/SpaceShipConsoleScript.cs
@@ -7,10 +7,7 @@ public class SpaceShipConsoleScript : NetworkBehaviour
 {
     [AllowMutableSyncType] public SyncVar<bool> shipLanded;
     [AllowMutableSyncType] public SyncVar<bool> shipPending;
-
-    [SerializeField] private GameObject landingText;
-    [SerializeField] private GameObject takeOffText;
-    
+        
     #region ShipPendingRegion
     public void SetShipPending(bool value)
     {
