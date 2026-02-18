a155d0b3f35472dcb780bd4e330a0b776a9f95ea
SpeedAmigo <Kxipi7@gmail.com>
2026-02-18 15:58:33 +0100

    status effect change test

--- Assets/Scripts/Other/StatusEffectHandler.cs
+++ Assets/Scripts/Other/StatusEffectHandler.cs
@@ -6,29 +6,49 @@ using UnityEngine;
 
 public class StatusEffectHandler : NetworkBehaviour
 {
-    public List<StatusEffectInstance> activeEffects;
-    //[AllowMutableSyncType] private SyncList<StatusEffectInstance> activeEffects = new();
+    [AllowMutableSyncType] private SyncList<StatusEffectData> activeEffects = new();
+    
+    private List<StatusEffectInstance> _serverInstances = new();
     
     [ServerRpc(RequireOwnership = false)]
     public void ApplyEffect(StatusEffect effect)
     {
-        StatusEffectInstance instance = new StatusEffectInstance(effect, this);
-        activeEffects.Add(instance);
+        var data = StatusEffectData.FromEffect(effect);
+        activeEffects.Add(data);
+        
+        var instance = new StatusEffectInstance(effect, this);
         instance.OnApply();
+        
+        _serverInstances.Add(instance);
     }
     
     private void Update()
     {
         if (!IsServerInitialized) return;
         
+        for (int i = _serverInstances.Count - 1; i >= 0; i--)
+        {
+            var inst = _serverInstances[i];
+            inst.Tick(Time.deltaTime);
+
+            if (inst.IsFinished)
+            {
+                inst.OnExpire();
+                _serverInstances.RemoveAt(i);
+                RemoveMatchingData(inst.effect);
+            }
+        }
+    }
+    
+    private void RemoveMatchingData(StatusEffect effect)
+    {
+        var tn = effect.GetType().AssemblyQualifiedName;
         for (int i = activeEffects.Count - 1; i >= 0; i--)
         {
-            activeEffects[i].Tick(Time.deltaTime);
-
-            if (activeEffects[i].IsFinished)
+            if (activeEffects[i].typeName == tn)
             {
-                activeEffects[i].OnExpire();
                 activeEffects.RemoveAt(i);
+                break;
             }
         }
     }
--- Assets/Scripts/Other/StatusEffectSerializer.cs
+++ Assets/Scripts/Other/StatusEffectSerializer.cs
@@ -4,54 +4,50 @@ using UnityEngine;
 
 public static class StatusEffectSerializer
 {
+        public static void WriteStatusEffectData(this Writer writer, StatusEffectData data)
+    {
+        writer.WriteString(data.typeName ?? string.Empty);
+        writer.WriteSingle(data.duration);
+    }
+
+    public static StatusEffectData ReadStatusEffectData(this Reader reader)
+    {
+        var typeName = reader.ReadString();
+        var duration = reader.ReadSingle();
+        return new StatusEffectData(typeName, duration);
+    }
+    
     public static void WriteStatusEffect(this Writer writer, StatusEffect statusEffect)
     {
-        if (statusEffect == null)
-        {
-            writer.WriteBoolean(false);
-            return;
-        }
+        if (writer == null) return;
         
-        writer.WriteBoolean(true);
+        bool hasValue = statusEffect != null;
+        writer.WriteBoolean(hasValue);
+        if (!hasValue) return;
         
-        writer.WriteString(statusEffect.GetType().AssemblyQualifiedName);
+        string typeName = statusEffect.GetType().AssemblyQualifiedName ?? string.Empty;
+        writer.WriteString(typeName);
         writer.WriteSingle(statusEffect.duration);
     }
 
     public static StatusEffect ReadStatusEffect(this Reader reader)
     {
+        if (reader == null) return null;
+        
         bool hasValue = reader.ReadBoolean();
         if (!hasValue) return null;
         
         string typeName = reader.ReadStringAllocated();
         float duration = reader.ReadSingle();
         
-        if (string.IsNullOrEmpty(typeName))
-            return null;
+        if (string.IsNullOrEmpty(typeName)) return null;
         
         Type t = Type.GetType(typeName, throwOnError: false);
-        if (t == null || !typeof(StatusEffect).IsAssignableFrom(t))
-            return null;
+        if (t == null || !typeof(StatusEffect).IsAssignableFrom(t)) return null;
 
         var effect = (StatusEffect)ScriptableObject.CreateInstance(t);
         effect.duration = duration;
-        // TODO: read additional fields into 'effect' if needed.
         
         return effect;
-    }
-
-    public static void WriteStatusEffectInstance(this Writer writer, StatusEffectInstance statusEffectInstance)
-    {
-        writer.WriteStatusEffect(statusEffectInstance?.effect);
-    }
-    
-    public static StatusEffectInstance ReadStatusEffectInstance(this Reader reader, StatusEffectHandler target)
-    {
-        var effect = reader.ReadStatusEffect();
-        if (effect == null)
-            return null;
-
-        var instance = new StatusEffectInstance(effect, target);
-        return instance;
     }
 }
