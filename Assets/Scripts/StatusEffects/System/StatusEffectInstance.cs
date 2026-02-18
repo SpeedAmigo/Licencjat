9e3bb8e9870d82949220b390cdc4e36b2f9ce4d2
SpeedAmigo <Kxipi7@gmail.com>
2026-02-18 16:19:14 +0100

    folders changes

---
+++ Assets/Scripts/StatusEffects/System/StatusEffectInstance.cs
@@ -0,0 +1,36 @@
+using FishNet.Object;
+using UnityEngine;
+
+[System.Serializable]
+public class StatusEffectInstance
+{
+    public StatusEffect effect;
+    
+    private StatusEffectHandler _target;
+    private float _timer;
+    
+    public bool IsFinished => _timer >= effect.duration;
+
+    public StatusEffectInstance(StatusEffect effect, StatusEffectHandler target)
+    {
+        this.effect = effect;
+        _target = target;
+        _timer = 0f;
+    }
+    
+    [Server]
+    public void OnApply() => effect.OnApply(this);
+    
+    [Server]
+    public void Tick(float deltaTime)
+    {
+        _timer += deltaTime;
+        effect.OnTick(this, deltaTime);
+    }
+    
+    [Server]
+    public void OnExpire() => effect.OnExpire(this);
+    
+    public T GetComponent<T>() => _target.GetComponent<T>();
+    public bool TryGetComponent<T>(out T component) => _target.TryGetComponent<T>(out component);
+}
