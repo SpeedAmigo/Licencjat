6cd12f0eee21fc29ef2fc8bc8594f1cc3da862ef
SpeedAmigo <Kxipi7@gmail.com>
2026-04-27 15:14:01 +0200

    shop rework done

--- Assets/Scripts/BasketItemUIScript.cs
+++ Assets/Scripts/BasketItemUIScript.cs
@@ -1,11 +1,18 @@
 using FishNet.Object;
+using FMODUnity;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BasketItemUIScript : NetworkBehaviour
+public class BasketItemUIScript : NetworkBehaviour, IInteractable
 {
+    [SerializeField] private string interactionText;
     [SerializeField] private Image image;
+
+    [Header("Sounds")]
+    [SerializeField] private EventReference removeSound;
+    
+    private string _itemID;
     
     public override void OnStartClient()
     {
@@ -17,6 +24,21 @@ public class BasketItemUIScript : NetworkBehaviour
     {
         gameObject.SetActive(true);
 
+        _itemID = null;
+        _itemID = itemData.ItemID;
         image.sprite = itemData.itemIcon;
+    }
+
+    public void Interact(PlayerRoot playerRoot)
+    {
+        if (!ShopManagerScript.Instance || !IsController) return;
+        
+        ShopManagerScript.Instance.RemoveItemFromBasket(_itemID);
+        SoundCreator.Instance.PlayOneShotAttached(removeSound, gameObject);
+    }
+
+    public string GetInteractText()
+    {
+        return interactionText;
     }
 }
--- Assets/Scripts/ShopItemButtonScript.cs
+++ Assets/Scripts/ShopItemButtonScript.cs
@@ -6,26 +6,17 @@ using UnityEngine;
 public class ShopItemButtonScript : NetworkBehaviour, IInteractable
 {
     [SerializeField] private string interactText;
-    [SerializeField] private bool addToBasket; 
     [SerializeField] private ShopItemUIScript shopItemUIScript;
 
+    [Header("Sounds")]
     [SerializeField] private EventReference addToBasketSound;
-    [SerializeField] private EventReference removeFromBasketSound;
     
     public void Interact(PlayerRoot playerRoot)
     {
         if (!ShopManagerScript.Instance || !IsController) return;
         
-        if (addToBasket)
-        {
-            ShopManagerScript.Instance.AddItemToBasket(shopItemUIScript.ItemId);
-            SoundCreator.Instance.PlayOneShot(addToBasketSound, transform.position);
-        }
-        else
-        {
-            ShopManagerScript.Instance.RemoveItemFromBasket(shopItemUIScript.ItemId);
-            SoundCreator.Instance.PlayOneShot(removeFromBasketSound, transform.position);
-        }
+        ShopManagerScript.Instance.AddItemToBasket(shopItemUIScript.ItemId);
+        SoundCreator.Instance.PlayOneShot(addToBasketSound, transform.position);
     }
 
     public string GetInteractText()
